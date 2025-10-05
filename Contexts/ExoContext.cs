c605025e2157e7b827bfa100d7410370d7a46bc7
LucasSantosMilhome <lucassantosmilhome@gmail.com>
2025-10-05 13:14:03 -0300

    Commit4

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,27 @@
+using System.Data.SqlClient;
+using Exo.WebApi.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext() { }
+
+        public ExoContext(DbContextOptions<ExoContext> options)
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=localhost\\SQLEXPRESS01;Database=ExoApi;Trusted_Connection=True;"
+                );
+            }
+        }
+
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
